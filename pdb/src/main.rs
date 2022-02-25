7f6e653a1d2a05aae9ad560007c7e9d17bde911f
theArctesian <stephen.d.okita@gmail.com>
2022-02-25 20:18:24 +0800

    save

--- pdb/src/main.rs
+++ pdb/src/main.rs
@@ -1,15 +1,31 @@
 use std::io;
+use std::time::{SystemTime};
 
 fn main() {
-    println!("Guess the number!");
 
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
+    println!("Input your activity:");
+    
+    // Activity related input
+    let mut activity = String::new();
+    let mut time = SystemTime::now();
+    let mut timespent = i32
+    let mut location = String::new();
+    // Emotional Input
+    let mut productivity = i32;
+    let mut stress = i32;
+    let mut happiness = i32;
+    let mut energy = i32;
+    let mut interest = i32;
     io::stdin()
-        .read_line(&mut guess)
+        .read_line(&mut activity)
         .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+    println!("You guessednum: {}", activity);
+//
+//    let mut two = String::new();
+//    io:stdin()
+//        .read_line(&mut two)
+//        .expect("Failed to read line");
+//
+//    println!("You gu {}", two);
 }
