b5b97e934b76885a8c047dc31e221552dfbba12d
TheArctesian <stephen.d.okita@gmail.com>
2022-03-02 00:25:38 +0800

    models

---
+++ pdb/src/model.rs
@@ -0,0 +1,16 @@
+//will need to setup diesel    
+
+
+pub struct Record {
+    pub id: i32,
+    pub date : String,
+    pub time : String, 
+    pub timeSpent : i32,
+    pub location : String,
+    pub Productivity : i32,
+    pub Stress : i32,
+    pub Happiness : i32,
+    pub Energy : i32,
+    pub Interest : i32,
+    pub SocialInteraction : bool
+}
